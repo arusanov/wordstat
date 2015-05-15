608b8a8b7bfc5930e05b3bded674b2c014fad968
arusanov <alexander.rusanov@gmail.com>
2015-05-15 12:20:32 +0300

    added sample test

---
+++ sortedkeys_test.go
@@ -0,0 +1,24 @@
+package main
+
+import "testing"
+
+func TestSortedKeys(t *testing.T) {
+    toSort := map[string]int{
+        "two":50,
+        "three":0,
+        "one":100,
+    }
+    keysShouldBe := []string{"one", "two", "three"}
+    keys := sortedKeys(toSort)
+    if len(keysShouldBe) != len(keys) {
+        t.Error("len should match")
+    }
+
+    for i := range keys {
+        if keys[i] != keysShouldBe[i] {
+            t.Error("expected ",keysShouldBe[i]," got ",keys[i])
+        }
+    }
+}
+
+
